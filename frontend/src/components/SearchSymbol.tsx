6bde64496e7e3386daf1f531fc022683693bf08f
Christian Kozalla <christian.kozalla@gmail.com>
2024-01-12 22:18:55 +0100

    primereact autocomplete

--- frontend/src/components/SearchSymbol.tsx
+++ frontend/src/components/SearchSymbol.tsx
@@ -1,4 +1,4 @@
-import { type CSSProperties, SyntheticEvent, useContext } from "react";
+import { type CSSProperties, useContext, useState } from "react";
 import { SeriesContext } from "../context/SeriesContext";
 import { AuthContext } from "../context/AuthContext";
 import { useFetch } from "../lib/hooks/useFetch";
@@ -8,13 +8,19 @@ import symbols from "../../../app/worker/alpaca/symbols.json";
 export function SearchSymbol({ style }: { style: CSSProperties }) {
   const { setSeries } = useContext(SeriesContext);
   const authContext = useContext(AuthContext);
+  const [selectedSymbol, setSelectedSymbol] = useState<string | null>(null);
+  const [suggestedSymbols, setSuggestedSymbols] = useState<string[]>([]);
   const { fetch } = useFetch(authContext);
 
-  const submitSymbol = async (_event: SyntheticEvent, symbol: string | null) => {
+  const submitSymbol = async (symbol: string | null) => {
     if (!symbol) {
       return;
     }
-    const [seriesRes, signalsRes] = await Promise.allSettled([fetch(`/api/symbols/${symbol.toUpperCase()}.json`), fetch(`/api/signals/${symbol}`)]);
+
+    const [seriesRes, signalsRes] = await Promise.allSettled([
+      fetch(`/api/symbols/${symbol.toUpperCase()}.json`),
+      fetch(`/api/signals/${symbol}`),
+    ]);
 
     if (seriesRes.status === 'fulfilled') {
       if (seriesRes.value?.status === 200) {
@@ -23,26 +29,28 @@ export function SearchSymbol({ style }: { style: CSSProperties }) {
 
         setSeries({ symbol, data, signals });
       } else if (seriesRes.value?.status === 404) {
+        // Handle not found
       }
     }
   };
 
-  const filterOptions = (options: string[], { inputValue }: { inputValue: string }) => {
-    const input = inputValue.toUpperCase();
-    const extactMatch = options.find((o) => o === input);
-    if (extactMatch) return [extactMatch];
-    else return options.filter((o) => o.includes(input)).sort((a) => a.startsWith(input) ? -1 : 1);
-  };
+  return (<AutoComplete
+    value={selectedSymbol}
+    onChange={(e) => setSelectedSymbol(e.value)}
+    suggestions={suggestedSymbols}
+    completeMethod={(event) => {
+      const input = event.query.toUpperCase();
+      const exactMatch = symbols.find((o) => o === input);
 
-  return (
-    <AutoComplete
-      type="text"
-      placeholder="Symbol e.g. AAPL"
-      suggestions={symbols}
-      onChange={submitSymbol}
-      filterOptions={filterOptions}
-      autoFocus
-      style={style}
-      sx={{ maxWidth: 300 }} />
-  );
+      if (exactMatch) setSuggestedSymbols([exactMatch]);
+      else
+        setSuggestedSymbols(symbols
+          .filter((o) => o.includes(input))
+          .sort((a, b) => (a.startsWith(input) ? -1 : b.startsWith(input) ? 1 : 0)));
+    }}
+    onSelect={(e) => submitSymbol(e.value)}
+    placeholder="Symbol e.g. AAPL"
+    autoFocus
+    style={style}
+  />);
 }
